0062efa4c8e1e88168d5db2bcdfc521a7233357b
Austin Heller <me@aheller.com>
2023-12-15 10:32:20 -0600

    Slightly improve dimension setter

--- src/editor/sequence/general/setSequenceDimensions.ts
+++ src/editor/sequence/general/setSequenceDimensions.ts
@@ -1,22 +1,24 @@
 export default function setSequenceDimensions() {
-  const sequenceEl = document.querySelector("#sequence");
+  const sequenceEl = document.querySelector("#sequence") as HTMLElement;
   if (!sequenceEl) return;
-  const sequenceEls = sequenceEl.querySelectorAll("#sequence .nodes .node");
+  const sequenceEls = sequenceEl.querySelectorAll("#sequence .nodes .node *");
   let width = 0,
     height = 0;
   sequenceEls.forEach((el: Element) => {
-    // @ts-ignore
-    const elWidth = el.offsetLeft + el.offsetWidth;
-    if (elWidth > width) {
-      width = elWidth;
-    } // @ts-ignore
-    const elHeight = el.offsetTop + el.offsetHeight;
-    if (elHeight > height) {
-      height = elHeight;
+    const rect = el.getBoundingClientRect();
+    const elTop = rect.top;
+    const elLeft = rect.left;
+    const elWidth = rect.width;
+    const elHeight = rect.height;
+    const elX = elLeft + elWidth;
+    if (elX > width) {
+      width = elX;
+    }
+    const elY = elTop + elHeight;
+    if (elY > height) {
+      height = elY;
     }
   });
-  // @ts-ignore
   sequenceEl.style.width = width + 50 + "px";
-  // @ts-ignore
   sequenceEl.style.height = height + 50 + "px";
 }
