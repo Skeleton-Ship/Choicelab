0b0df46dc6c64ef4b165a39f0fa9b55e436bfc8f
Austin Heller <me@aheller.com>
2023-12-25 23:22:55 -0600

    Accommodate scroll position in sizing

--- src/editor/sequence/general/setSequenceDimensions.ts
+++ src/editor/sequence/general/setSequenceDimensions.ts
@@ -1,13 +1,14 @@
 export default function setSequenceDimensions() {
   const sequenceEl = document.querySelector("#sequence") as HTMLElement;
-  if (!sequenceEl) return;
+  const sequenceWrap = document.querySelector("#sequence-wrap") as HTMLElement;
+  if (!sequenceEl || !sequenceWrap) return;
   const sequenceEls = sequenceEl.querySelectorAll("#sequence .nodes .node *");
   let width = 0,
     height = 0;
   sequenceEls.forEach((el: Element) => {
     const rect = el.getBoundingClientRect();
-    const elTop = rect.top;
-    const elLeft = rect.left;
+    const elTop = rect.top + sequenceWrap.scrollTop;
+    const elLeft = rect.left + sequenceWrap.scrollLeft;
     const elWidth = rect.width;
     const elHeight = rect.height;
     const elX = elLeft + elWidth;
