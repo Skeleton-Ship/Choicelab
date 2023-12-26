915af12f0916659d3ee72658b7adf5ef28f4a143
Austin Heller <me@aheller.com>
2023-12-26 17:15:05 -0600

    Use vibrancy effect on windows on macOS

--- src-tauri/src/ui_operations/create_launcher.rs
+++ src-tauri/src/ui_operations/create_launcher.rs
@@ -1,5 +1,7 @@
 use tauri::Window;
 use tauri::TitleBarStyle;
+use tauri::Manager;
+use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
 
 pub fn create_launcher(app: &tauri::App) -> Window {
 	
@@ -10,8 +12,14 @@ pub fn create_launcher(app: &tauri::App) -> Window {
 	.resizable(false)
 	.title_bar_style(TitleBarStyle::Overlay.clone())
 	.title("")
+	.transparent(true)
 	.build()
 	.unwrap();
+	
+	// Apply vibrancy to launcher
+	let window = app.get_window("launcher").unwrap();
+	#[cfg(target_os = "macos")]
+	apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None).expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");	
 	
 	return launcher;
 }
--- src/fs/loadProject.ts
+++ src/fs/loadProject.ts
@@ -10,6 +10,7 @@ export default function loadProject(projectPath: string) {
     height: 700,
     minWidth: 600,
     minHeight: 360,
+    transparent: true,
   });
   webview.once("tauri://created", function () {});
   // webview.once("tauri://error", function () {});
