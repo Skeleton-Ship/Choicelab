cd67971672785ce55e1684fe7a8f2924774c42cb
Austin Heller <me@aheller.com>
2025-07-03 23:44:58 -0600

    Format date when copying log

--- changelog.cjs
+++ changelog.cjs
@@ -5,7 +5,7 @@ const changelogPath = path.join(__dirname, "CHANGELOG.md");
 const outputPath = path.join(__dirname, "src", "whats-new", "WhatsNew.md");
 
 if (!fs.existsSync(changelogPath)) return;
-const changelog = fs.readFileSync(changelogPath, "utf8");
+let changelog = fs.readFileSync(changelogPath, "utf8");
 const h2Regex = /^##\s.*$/gm;
 const matches = changelog.match(h2Regex);
 if (matches.length === 0) return;
@@ -13,6 +13,22 @@ const firstH2Index = changelog.indexOf(matches[0]);
 const nextH2Index = matches[1]
   ? changelog.indexOf(matches[1])
   : changelog.length - 1;
+  const dateRegex = /#### \d\d\d\d-[\dx][\dx]-[\dx][\dx]/g;
+const dateMatches = changelog.match(dateRegex);
+if (dateMatches) {
+  const match = dateMatches[0];
+  const dateString = match.replace("#### ", "");
+  const centralDateString = `${dateString}T00:00:00-06:00`;
+  const date = new Date(centralDateString);
+  if (!isNaN(date.getTime())) {
+    const formattedDate = date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    });
+    changelog = changelog.replace(match, `#### ${formattedDate}`);
+  }
+}
 const whatsNewContent = changelog.substring(firstH2Index, nextH2Index).trim();
 fs.writeFileSync(outputPath, whatsNewContent, "utf8");
 return whatsNewContent;
