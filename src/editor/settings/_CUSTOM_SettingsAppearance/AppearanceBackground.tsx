4647aca9865ef506fb197b872443e209629764cf
Austin Heller <me@aheller.com>
2026-02-01 12:07:05 -0600

    Remove debug logging

--- src/editor/settings/_CUSTOM_SettingsAppearance/AppearanceBackground.tsx
+++ src/editor/settings/_CUSTOM_SettingsAppearance/AppearanceBackground.tsx
@@ -5,7 +5,6 @@ export function AppearanceBackground(props: {
   initial: any;
   update: (key: string, newValues: { [key: string]: any }) => void;
 }) {
-  console.log(props.initial);
   const [kind, setKind] = useState(props.initial.background.kind);
   return (
     <>
--- src/editor/settings/_CUSTOM_SettingsAppearance/AppearanceInputs.tsx
+++ src/editor/settings/_CUSTOM_SettingsAppearance/AppearanceInputs.tsx
@@ -7,7 +7,6 @@ export function AppearanceInputs(props: {
   initial: any;
   update: (key: string, newValues: { [key: string]: any }) => void;
 }) {
-  console.log(props.initial);
   return (
     <>
       <div class="section font">
